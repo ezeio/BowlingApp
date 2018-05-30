bea873f84a2b762bde612ebf6bd2a3e6b7dd8774
Ubuntu Eze <ezeosy@gmail.com>
2018-05-30 14:53:06 +0100

    added new cucumber dependancies to the pom file

--- src/test/java/CukesRunner.java
+++ src/test/java/CukesRunner.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.*;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources"})
 public class CukesRunner {
+
 }
